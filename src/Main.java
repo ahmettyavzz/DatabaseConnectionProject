c0d0ce8e797195116b79b95881ae2e47cfb78de2
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-18 13:21:28 +0300

    Main.Java updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import DataAccess.abstracts.Database;
+import DataAccess.concretes.MySQL;
+import DataAccess.concretes.Oracle;
+import DataAccess.concretes.PostgreSQL;
+import Model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Product> products= new ArrayList<>();
+
+
+        // Database myOracle= new Oracle(products);
+        // myOracle.connection();
+
+        Database mysql= new MySQL(products);      //arrayi doldurma
+        mysql.connection();
+
+        Database myPostgre= new PostgreSQL(products);       //arrayi postgreye atma
+        myPostgre.connection();
+
+
+
     }
 }
