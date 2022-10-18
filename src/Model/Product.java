73aad033a463ad1dcdae7e14e6d57bf3b894cfc7
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-10-18 12:39:42 +0300

    Product.java created for orm

---
+++ src/Model/Product.java
@@ -0,0 +1,42 @@
+package Model;
+
+
+public class Product {
+    private int productId;
+    private String productName;
+    private String category;
+
+    public Product() {
+    }
+
+    public Product(int productId, String productName, String category) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+}
